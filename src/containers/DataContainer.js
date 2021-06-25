950cc1d14532a4b6b152976453db95cfd261b184
sara <smhumphries@hotmail.co.uk>
2021-06-25 14:38:08 +0100

    30 day data starts from 1 day ago

--- src/containers/DataContainer.js
+++ src/containers/DataContainer.js
@@ -17,7 +17,7 @@ const DataContainer = function() {
     const fetchDataAPI = function () {
         const dataAPI = fetch('https://api.coronavirus.data.gov.uk/v1/data')
             .then(result => result.json())
-            .then(API => setDataAPI(API.data.splice(0,30)));
+            .then(API => setDataAPI(API.data.splice(1,30)));
         
     };
 
